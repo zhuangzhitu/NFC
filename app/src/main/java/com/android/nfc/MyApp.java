818199fa300526f135b28d66ef2019b004fcd7cd
zhihao.zhang <605370697@qq.com>
2017-02-16 19:29:54 +0800

    Initial commit

---
+++ app/src/main/java/com/android/nfc/MyApp.java
@@ -0,0 +1,16 @@
+package com.android.nfc;
+
+import android.app.Application;
+
+/**
+ * @author Created by zhihao on 2017/2/7.
+ * @describe
+ * @version_
+ **/
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
